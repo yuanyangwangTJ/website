7f30b5e6ecb3503ce4a0553c85b977f4bd411da3
MaxMorning <hml0814@163.com>
2020-12-19 01:05:59 +0800

    Score display complete
    Score display complete.

--- app/database.py
+++ app/database.py
@@ -10,18 +10,24 @@ UPLOAD_FOLDER = '../upload'
 class User(db.Model):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
-    usertype = db.Column(db.Enum('student', 'teacher'),
+    usertype = db.Column(db.Enum('student', 'teacher', 'admin'),
                      nullable=False, default='student')
     password = db.Column(db.Text, nullable=False)
     profile_image_path = db.Column(db.String(255))
     profile_image_name = db.Column(db.String(255))
+
+#存放报名信息
+tags = db.Table('tags',
+    db.Column('User_id', db.Integer, db.ForeignKey('User.id')),
+    db.Column('Activity_id', db.Integer, db.ForeignKey('Activity.id'))
+)
 
 class Activity(db.Model):
     __tablename__ = 'Activity'
     id = db.Column(db.Integer, primary_key=True,
                    nullable=False, autoincrement=True)
     name = db.Column(db.String(255), nullable=False)
-    discribe = db.Column(db.String(255))
+    description = db.Column(db.String(255))
     cover_image_path = db.Column(db.String(255))
     cover_image_name = db.Column(db.String(255))
     label = db.Column(db.Enum('virtue', 'wisdom', 'body',
@@ -29,16 +35,20 @@ class Activity(db.Model):
 
     start_time = db.Column(db.DateTime, nullable=False)
     end_time = db.Column(db.DateTime, nullable=False)
-    leed_teacher = db.Column(db.String(255), nullable=False)
+    lead_teacher = db.Column(db.String(255), nullable=False)
     cover_path = db.Column(db.Text)
-    max_join = db.Column(db.Integer)
+    max_join = db.Column(db.Integer, default=999)
+    current_join = db.Column(db.Integer, default=0)
+    score = db.Column(db.Integer, default=1)
+    participants = db.relationship('User', secondary=tags,
+        backref=db.backref('activities', lazy='dynamic'))
+    status = db.Column(db.Enum('coming', 'finished'), default='coming')
 
 
-
-class Apply(db.Model):
+""" class Apply(db.Model):
     __tablename__ = 'Apply'
     id = db.Column(db.Integer, primary_key=True,
                    nullable=False, autoincrement=True)
     userid = db.Column(db.Integer, nullable=False)
     actid = db.Column(db.Integer, nullable=False)
-    #status = (db.Enum('apply', 'access'), nullable=False)
+    #status = (db.Enum('apply', 'access'), nullable=False) """
--- app/space.py
+++ app/space.py
@@ -1,9 +1,26 @@
 import os
 import datetime
 from flask import Flask, Blueprint, url_for, render_template, request, make_response, redirect, abort, session, flash, g
+from flask_sqlalchemy import SQLAlchemy
 
 from app.auth import login_required
 from app.database import db, User, Activity, UPLOAD_FOLDER
+
+def stat_scores(student):
+    """
+    count virtue score
+    :param student: student, a User instance
+    :return: dict: total scores, None means student don't exist
+    """
+    if student.usertype != 'student':
+        return None
+
+    scores = {'virtue': 0, 'wisdom': 0, 'body': 0, 'beauty': 0, 'labor': 0}
+    for activity in student.activities:
+        if activity.status == 'finished' and activity.label in scores:
+            scores[activity.label] += activity.score
+
+    return scores
 
 
 bp = Blueprint('space', __name__)
@@ -14,7 +31,7 @@ def personal_page():
     user_id = session['user_id']
     user = User.query.filter(User.id == user_id).first()
     if not user:
-        return render_template('user.html', user_id=None, profile_image_path=None, user=None)
+        return render_template('./pages/user.html', user_id=None, profile_image_path=None, user=None, scores={'virtue': 0, 'wisdom': 0, 'body': 0, 'beauty': 0, 'labor': 0})
 
     if not (user.profile_image_path and user.profile_image_name):
         profile_image_path = None
@@ -22,7 +39,9 @@ def personal_page():
         profile_image_path = os.path.join(
         user.profile_image_path, user.profile_image_name)
 
-    return render_template('user.html', user_id=user_id, profile_image_path=profile_image_path, user=user)
+    scores = stat_scores(user)
+    total_score = scores['virtue'] + scores['wisdom'] + scores['body'] + scores['beauty'] + scores['labor']
+    return render_template('./pages/user.html', user_id=user_id, profile_image_path=profile_image_path, user=user, scores=scores, total_score=total_score)
 
 
 # TO-DO need to build socre display part according to the pattern in user.html
