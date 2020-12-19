505049f518e97fd5e94f12fa8de5585610b26276
MaxMorning <hml0814@163.com>
2020-12-19 14:00:04 +0800

    activity detail without apply button complete
    Activity detail without apply button complete.
    -show activity name, description and cover image.

--- app/activity.py
+++ app/activity.py
@@ -28,23 +28,21 @@ def new():
 
 @bp.route('/<int:id>/', methods=['POST', 'GET'])
 def activity(id):
-    if (request.method == 'POST'):
-        if (session['usertype'] == 'admin'):
-            Activity.query.filter(Activity.id == id).delete()
-            Apply.query.filter(Apply.actid == id).delete()
-            db.session.commit()
-        elif (session['usertype'] == 'normal'):
-            apply = Apply.query.filter(
-                Apply.actid == id and Apply.userid == session['userid']).first()
-            if (apply == None):
-                apply = Apply(actid=id, userid=session['userid'])
-                db.session.add(apply)
-                db.session.commit()
     act = Activity.query.filter(Activity.id == id).first()
-    if(act == None):
+    if (request.method == 'POST'):
+        if (session['usertype'] != 'student'):#teacher和admin可以删除活动
+            act.delete()
+            db.session.commit()
+            return redirect(url_for("home"))
+        elif (session['usertype'] == 'student'):#studendt可以报名
+            student = User.query.filter(User.id == g.userid).first()#取当前用户的id？
+            if (student not in act.participants):#判断正确性存疑？
+                activity.participants.append(student)
+                db.session.commit()
+    if (act == None):
         return redirect(url_for("home"))
     args = {
         'session': session,
         'act': act
-    }
-    return render_template("activity.html", **args)
+        }
+    return render_template("activity-demo.html", **args, activity=act)
